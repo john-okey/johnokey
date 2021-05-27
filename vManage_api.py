3b2e6a756334ddfffd7c260aebd033b8bd265b12
johnokey <mr.john.okey@gmail.com>
2021-05-27 17:36:56 +1000

    Create vManage_api.py
    Initial auth to vManage

---
+++ vManage_api.py
@@ -0,0 +1,24 @@
+# credit: JunK
+#
+from getpass import getpass
+import json
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+vmanage = input("vManage UI uri: ")
+username = input("Username: ")
+password = getpass("Password: ")
+api = "dataservice/system/device/vedges"
+
+headers = {
+    'content-type': "application/json",
+}
+
+ url_request = "https://" + vmanage + "/" + api
+
+print (url_request)
+ 
+response = requests.get(url_request, auth=(username, password), verify=False)
+print (response.status_code)
+print(response.json())
